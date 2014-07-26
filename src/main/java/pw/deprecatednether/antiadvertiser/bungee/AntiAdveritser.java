53a484867b95a8ffa7d4756d143e8f7878c71010
DeprecatedNether <deprecatednether@gmail.com>
2014-07-26 14:13:00 +0200

    Initial commit

---
+++ src/main/java/pw/deprecatednether/antiadvertiser/bungee/AntiAdveritser.java
@@ -0,0 +1,10 @@
+package pw.deprecatednether.antiadvertiser.bungee;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+public class AntiAdveritser extends Plugin {
+    @Override
+    public void onEnable() {
+
+    }
+}
